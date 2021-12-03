d8248c417dcc6b95c2c55bb5a32ef85ac68e8694
Liu-Nian-TUBS <72320600+Liu-Nian-TUBS@users.noreply.github.com>
2021-12-03 13:02:28 +0100

    SG no need to input
    SG no need to input

--- warp_change_ip.sh
+++ warp_change_ip.sh
@@ -4,13 +4,9 @@
 #Blog https://ty.al
 
 UA_Browser="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"
-read -r -p "Is warp installed? [y/n] " input
-if [[ "$input" == "n" ]];then
-    bash <(curl -fsSL https://github.com/luoxue-bot/warp.sh/raw/main/warp.sh) 4
-elif [[ "$input" == "y" ]];then
-    read -r -p "Input the region you want(e.g. HK,SG):" area
-fi
-while [[ "$input" == "y" ]]
+
+area = "SG"
+while true
 do
     result=$(curl --user-agent "${UA_Browser}" -fsL --write-out %{http_code} --output /dev/null --max-time 10 "https://www.netflix.com/title/81215567" 2>&1)
     if [[ "$result" == "404" ]];then
@@ -34,7 +30,7 @@ do
             sleep 3
         else
             echo -e "Region: ${region} Done, monitoring..."
-            sleep 6
+            sleep 300
         fi
 
     elif  [[ "$result" == "000" ]];then
