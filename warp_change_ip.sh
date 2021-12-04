495941ef39e50ef5de204b9f55e8a4b542cae4d9
Liu-Nian-TUBS <72320600+Liu-Nian-TUBS@users.noreply.github.com>
2021-12-04 20:10:45 +0100

    Update warp_change_ip.sh

--- warp_change_ip.sh
+++ warp_change_ip.sh
@@ -1,10 +1,5 @@
-#bin/bash!
-
-#Github @luoxue-bot
-#Blog https://ty.al
-
 UA_Browser="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"
-area = "SG"
+area="SG"
 while true
 do
     result=$(curl --user-agent "${UA_Browser}" -fsL --write-out %{http_code} --output /dev/null --max-time 10 "https://www.netflix.com/title/81215567" 2>&1)
